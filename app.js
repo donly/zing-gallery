8af19a9818461b85d217d256458954d75f034b4b
Dong <donly@magicalboy.com>
2019-03-24 23:48:54 +0800

    catch err

--- app.js
+++ app.js
@@ -22,5 +22,15 @@ app.use('/', require('./lib/gallery.js')(Object.assign({
   }, cfg));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 app.listen(port, () => console.log(`Listening on ${ port }`));
